4622ddb2e81099b8ff0081f452168c0ae0da309e
Sushant Bangalorkar <sushant17784@gmail.com>
2021-03-19 16:07:07 +0530

    first chunck : props , createElement , JSX restrictions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Hi, I am a React App</h1>  
+      <Person name="Max" age="24"/>
+      <Person name="Manu" age="27"/>
+      <Person name="Steph" age="22"/>
       </div>
     );
+    // const child = React.createElement('h1',null,'Hi, I\'m a React App');
+    // return React.createElement('div',{className: 'App'}, child);
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const person = (props) =>{
+return (
+    <div>I am a {props.name} and I am {props.age} years old.</div>
+)
+};
+
+export default person;
